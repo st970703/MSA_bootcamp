e2445c2c78d85f2775568a802f59f71906d8b28a
Mike Lee <elee353@aucklanduni.ac.nz>
2016-08-06 02:55:17 +1200

    fist commit

---
+++ js/musichandler.js
@@ -0,0 +1,7 @@
+// var myClientId = "6e8c83ca66867e5378bfcbb8b3181685";
+// function initSC() : void {
+//     // init SoundCloud
+//     SC.initialize({
+//         client_id: myClientId
+//     });
+// } 
